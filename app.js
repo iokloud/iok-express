f8ff49aa68be59c6fb159424d268cceed1eff1ab
ioBoard <hmahdavi@neda.net>
2017-08-24 12:14:34 -0700

    add bin\www

--- app.js
+++ app.js
@@ -4,35 +4,26 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var morgan = require('morgan');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var config = require('./config/database');
-var index = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
 
 mongoose.connect(config.database);
 
 var api = require('./routes/api');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-app.use(passport.initialize());
-
-app.get('/', function(req, res) {
-  res.send('Page under construction.');
-});
-
-app.use('/api', api);
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -41,9 +32,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('dev'));
+app.use(passport.initialize());
 
-app.use('/', index);
-app.use('/users', users);
+app.get('/', function(req, res) {
+  res.send('Page under construction.');
+});
+
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
